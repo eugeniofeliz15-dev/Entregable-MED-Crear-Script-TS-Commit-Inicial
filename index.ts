907481f99eaa4d0d6e7e6b394475c6c63b7616d9
CristianClaudio <eugenio.feliz15@gmail.com>
2026-08-10 19:25:24 -0400

    feat: setup inicial y variables globales

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
+import readline from 'readline/promises';
+import { stdin as input, stdout as output } from 'process';
+
+const rl = readline.createInterface({ input, output });
+// 🚫 No eliminar las líneas de arriba ⬆️
+
 // ✍️ Escribe tu código aquí 👇
 let systemName: string = "Digibros";
 console .log (systemName);
@@ -15,3 +21,5 @@ let sueldoPorHora: number = 250;
 
 console .log (horasTrabajadas * sueldoPorHora);
 
+// 🚫 No eliminar las líneas de abajo ⬇️
+rl.close();
