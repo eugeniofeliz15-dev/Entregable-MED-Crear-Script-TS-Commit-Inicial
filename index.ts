be748327a7740e26089329e1c1dec11b52ea17e5
CristianClaudio <eugenio.feliz15@gmail.com>
2026-08-10 19:32:04 -0400

    Se elimino el error

--- index.ts
+++ index.ts
@@ -1,9 +1,3 @@
-import readline from 'readline/promises';
-import { stdin as input, stdout as output } from 'process';
-
-const rl = readline.createInterface({ input, output });
-// 🚫 No eliminar las líneas de arriba ⬆️
-
 // ✍️ Escribe tu código aquí 👇
 let systemName: string = "Digibros";
 console .log (systemName);
@@ -21,5 +15,3 @@ let sueldoPorHora: number = 250;
 
 console .log (horasTrabajadas * sueldoPorHora);
 
-// 🚫 No eliminar las líneas de abajo ⬇️
-rl.close();
