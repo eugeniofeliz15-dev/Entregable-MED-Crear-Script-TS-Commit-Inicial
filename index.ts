1ed4debfb1359d0f293b1c957178ade85f12046c
CristianClaudio <eugenio.feliz15@gmail.com>
2026-08-10 19:22:29 -0400

    first commit

---
+++ index.ts
@@ -0,0 +1,17 @@
+// ✍️ Escribe tu código aquí 👇
+let systemName: string = "Digibros";
+console .log (systemName);
+let version: number  = 2.14;
+console .log (version);
+let userName: string = "Carlos";
+console .log (userName);
+let Sueldo: string = "Sueldo por dia";
+console .log (Sueldo);
+
+// Horas trabajadas * sueldo
+
+let horasTrabajadas: number = 8;
+let sueldoPorHora: number = 250;
+
+console .log (horasTrabajadas * sueldoPorHora);
+
