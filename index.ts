d646421d33abdf962b3cd97da65af53e155af824
CristianClaudio <eugenio.feliz15@gmail.com>
2026-08-12 18:33:55 -0400

    Cambio en los datos de entrada

--- index.ts
+++ index.ts
@@ -1,4 +1,11 @@
-// ✍️ Escribe tu código aquí 👇
+import readline from 'readline/promises';
+import { stdin as input, stdout as output } from 'process';
+
+const rl = readline.createInterface({ input, output });
+const answer = await rl.question("¿Cuál es tu nombre? ");
+console.log(`Hola, ${answer}!`);
+
+/* // ✍️ Escribe tu código aquí 👇
 let systemName: string = "Digibros";
 console .log (systemName);
 let version: number  = 2.14;
